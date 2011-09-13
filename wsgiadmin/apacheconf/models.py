8e6aff00defa4b208dc777223a2a0308296e5b08
yed_ <alemilostpane@tadyjekrakonosovo.cz>
2011-09-13 21:55:26 +0200

    minor code cleanup

--- wsgiadmin/apacheconf/models.py
+++ wsgiadmin/apacheconf/models.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from django.contrib.auth.models import User as user
 from django.db import models
-from django.forms import ModelForm
 from django import forms
 from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext_lazy as _
@@ -96,7 +94,7 @@ class Site(models.Model):
 		if self.owner.parms.fee:
 			return 0
 			
-		if self.type == "uwsgi" or self.type == "modwsgi":
+		if self.type in ("uwsgi", "modwsgi"):
 			return (settings.PAYMENT_WSGI[self.owner.parms.currency]/30.0)*self.owner.parms.dc()
 		elif self.type == "php":
 			return (settings.PAYMENT_PHP[self.owner.parms.currency]/30.0)*self.owner.parms.dc()
@@ -115,7 +113,7 @@ class form_static(forms.Form):
 class form_wsgi(forms.Form):
 	domains		= forms.CharField(label=_(u"Domény *"),help_text=_(u"<br />Domény na kterých bude web server naslouchat oddělené mezerou. Například 'rosti.cz www.rosti.cz ' apod. První doména je brána jako hlavní."))
 	static		= forms.CharField(label=_(u"Adresáře se statickými soubory"), widget=forms.Textarea, help_text=_(u"<br /><strong>~/&lt;zadaná_cesta&gt;</strong> - Jeden řádek, jeden adresář. Formát <strong>/url/ /cesta/k/mediím/</strong> - Odělovačem je mezera. Chybné řádky budou při generování konfigurace ignorovány."), required=False)
-	python_path	= forms.CharField(label=_(u"Python path"), widget=forms.Textarea, help_text=_(u"<br /><strong>~/&lt;zadaná_cesta&gt;</strong> - Jeden řádek, jeden adresář. Formát <strong>/tady/je/moje/aplikace</strong>. Váš domovský adresář bude automaticky doplněn. uWSGI ignoruje nastavení PYTHON_PATH přes sys.path."), required=False)
+	python_path	= forms.CharField(label=_(u"Python path"), widget=forms.Textarea, help_text=_(u"<br /><strong>~/&lt;zadaná_cesta&gt;</strong> - Jeden řádek, jeden adresář. Formát <strong>/tady/je/moje/aplikace</strong>. Váš domovský adresář bude automaticky doplněn.<br />uWSGI ignoruje nastavení PYTHON_PATH přes sys.path."), required=False)
 	virtualenv	= forms.ChoiceField(label=_(u"Virtualenv *"),choices=(("default","default"),), initial="default", help_text=_(u"<br />Pythoní virtuální prostředí. Najdete je ve '<strong>~/virtualenvs/&lt;zadaná_hodnota&gt;</strong>'. Můžete si si vytvořit vlastní přes SSH."), required=True)
 	script		= forms.ChoiceField(label=_(u"WSGI skript *"))
 	allow_ips	= forms.CharField(label=_(u"Povolené IPv4 adresy"), widget=forms.Textarea, help_text=_(u"<br />Jedna IP adresa na jeden řádek. Pokud je pole prázdné, fungují všechny."), required=False)

285b62579260d06a5c7d535da540a0426a9bb5c0
Jiri Suchan <jiri.suchan@centrumholdings.com>
2011-09-17 02:10:33 +0200

    code cleanup

---
+++ wsgiadmin/service/management/commands/commit.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from wsgiadmin.requests.request import SSHHandler
+from wsgiadmin.clients.models import Machine
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    help = "Commits performed user actions"
+
+    def handle(self, *args, **options):
+        m = Machine.objects.all()[0]
+        u = User.objects.all()[0]
+
+        sh = SSHHandler(u, m)
+        sh.commit()

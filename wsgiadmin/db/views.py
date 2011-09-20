7b2d3361b8ee71c27ab27824680fbf6e8df2d208
Jiri Suchan <jiri.suchan@centrumholdings.com>
2011-09-20 23:56:02 +0200

    pythonify

--- wsgiadmin/db/views.py
+++ wsgiadmin/db/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.core.paginator import Paginator
 from django.core.urlresolvers import reverse
@@ -8,7 +8,6 @@ from django.contrib.auth.decorators import login_required
 from django.template.context import RequestContext
 
 from django.utils.translation import ugettext_lazy as _, ugettext
-from django.views.decorators.csrf import csrf_exempt
 from wsgiadmin.apacheconf.views import JsonResponse
 
 from wsgiadmin.db.forms import PgsqlForm, MysqlForm

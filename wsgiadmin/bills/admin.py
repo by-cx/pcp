10068bdd18d87d08280caceea87504bc85f38aec
Jiri Suchan <jiri.suchan@centrumholdings.com>
2011-09-21 00:54:14 +0200

    chillout refactoring

--- wsgiadmin/bills/admin.py
+++ wsgiadmin/bills/admin.py
@@ -4,17 +4,19 @@ from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
 class bill_admin(admin.ModelAdmin):
-	list_display = ("pub_date","price","info","processed","user",)
-	list_display_links = ['pub_date']
+    list_display = ("pub_date", "price", "info", "processed", "user",)
+    list_display_links = ['pub_date']
+
 
 class service_admin(admin.ModelAdmin):
-	list_display = ("info","price","every","user",)
-	list_display_links = ('info',)
-		
-class income_admin(admin.ModelAdmin):
-	list_display = ("pub_date","user","cash", "note",)
-	list_display_links = ('pub_date',)
+    list_display = ("info", "price", "every", "user",)
+    list_display_links = ('info',)
 
-admin.site.register(income,income_admin)
-admin.site.register(bill,bill_admin)
-admin.site.register(service,service_admin)
+
+class income_admin(admin.ModelAdmin):
+    list_display = ("pub_date", "user", "cash", "note",)
+    list_display_links = ('pub_date',)
+
+admin.site.register(income, income_admin)
+admin.site.register(bill, bill_admin)
+admin.site.register(service, service_admin)

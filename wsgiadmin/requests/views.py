c0947c7514c1185599103838d95a1244e2fdd9f4
Adam Strauch <cx@initd.cz>
2011-09-21 18:19:37 +0200

    No machine or user for commit

--- wsgiadmin/requests/views.py
+++ wsgiadmin/requests/views.py
@@ -4,10 +4,7 @@ from wsgiadmin.clients.models import Machine
 from wsgiadmin.requests.request import SSHHandler
 
 def commit(request):
-    m = Machine.objects.all()[0]
-    u = User.objects.all()[0]
-
-    sh = SSHHandler(u, m)
+    sh = SSHHandler(None, None)
     sh.commit()
 
     return HttpResponse("OK")

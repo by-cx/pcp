61c1dfe92bde32fa2ec7d9a6380ff1298748fddb
Adam Strauch <cx@initd.cz>
2011-10-29 03:03:30 +0200

    JSON is JSON :-)

--- wsgiadmin/stats/management/commands/report.py
+++ wsgiadmin/stats/management/commands/report.py
@@ -1,36 +1,10 @@
 from datetime import date
+import json
 import pickle
 import re
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from wsgiadmin.stats.models import Record
-
-class Report(object):
-    def __init__(self):
-        self.webs = []
-        self.address = []
-
-    def __unicode__(self):
-        return u", ".join([web.domain for web in self.webs])
-    def __str__(self):
-        return ", ".join([web.domain for web in self.webs])
-
-class WebRecord(object):
-    def __init__(self):
-        self.date_start = None
-        self.date_end = None
-        self.service = ""
-        self.domain = ""
-        self.processes = 0
-
-    def __unicode__(self):
-        s = u"%s - %s\n" % (self.date_start, self.date_end)
-        s += u"%s %s" % (self.service, self.domain)
-        return s
-    def __str__(self):
-        s = "%s - %s\n" % (self.date_start, self.date_end)
-        s += "%s %s" % (self.service, self.domain)
-        return s
 
 class Command(BaseCommand):
     help = "Generate report for invoice system. Optionaly, you can give MM/YYYY format, otherwise get actual month"
@@ -55,37 +29,75 @@ class Command(BaseCommand):
             year = s.groups()[1]
         return month, year
 
+    def get_address(self, user):
+        if user.parms.address.invoice_name:
+            if len(user.parms.address.invoice_name.split(" ")) >= 2:
+                first_name = user.parms.address.invoice_name.split(" ")[0]
+                last_name = user.parms.address.invoice_name.split(" ")[1]
+            else:
+                first_name = user.parms.address.invoice_name.split(" ")[0]
+                last_name = ""
+            address = {
+                "company": user.parms.address.company,
+                "first_name": first_name,
+                "last_name": last_name,
+                "street": user.parms.address.invoice_street,
+                "city": user.parms.address.invoice_city,
+                "zip": user.parms.address.invoice_city_num,
+                "phone": user.parms.address.invoice_phone,
+                "email": user.parms.address.invoice_email,
+                "company_number": user.parms.address.residency_ic,
+                "vat_number": user.parms.address.residency_dic,
+            }
+        else:
+            if len(user.parms.address.residency_name.split(" ")) >= 2:
+                first_name = user.parms.address.residency_name.split(" ")[0]
+                last_name = user.parms.address.residency_name.split(" ")[1]
+            else:
+                first_name = user.parms.address.residency_name.split(" ")[0]
+                last_name = ""
+            address = {
+                "company": user.parms.address.company,
+                "first_name": first_name,
+                "last_name": last_name,
+                "street": user.parms.address.residency_city,
+                "city": user.parms.address.residency_city,
+                "zip": user.parms.address.residency_city_num,
+                "phone": user.parms.address.residency_phone,
+                "email": user.parms.address.residency_email,
+                "company_number": user.parms.address.residency_ic,
+                "vat_number": user.parms.address.residency_dic,
+            }
+        return address
+
     def handle(self, *args, **options):
         month, year = self.get_date(args)
 
         users = User.objects.all()
         reports = []
         for user in users:
-            report = Report()
+            report = {"webs": []}
             records = self.get_records(user, month, year)
             last = None
-            web_record = None
+            web_record = {}
             record = None
             for record in records:
                 if last and record.value != last.value:
                     if web_record:
-                        web_record.date_end = last.date
-                        report.webs.append(web_record)
-                    web_record = WebRecord()
-                    web_record.date_start = record.date
-                    web_record.date_end = record.date
-                    web_record.service = record.service
-                    web_record.domain = record.value
+                        web_record["date_end"] = pickle.dumps(last.date)
+                        report["webs"].append(web_record)
+                    web_record = {"date_start": pickle.dumps(record.date), "date_end": pickle.dumps(record.date), "service": record.service,
+                                  "domain": record.value}
                     proc = re.findall("\(([0-9]+) proc.\)", record.value)
                     if proc:
-                        web_record.processes = proc[0]
+                        web_record["processes"] = proc[0]
                 last = record
             if record and web_record:
-                web_record.date_end = record.date
-                report.webs.append(web_record)
-            report.address = user.parms.address
-            report.fee = user.parms.fee
-            report.discount = user.parms.discount
-            if report.webs:
+                web_record["date_end"] = pickle.dumps(record.date)
+                report["webs"].append(web_record)
+            report["address"] = self.get_address(user)
+            report["fee"] = user.parms.fee
+            report["discount"] = user.parms.discount
+            if report["webs"]:
                 reports.append(report)
-            print pickle.dumps(reports)
+            print json.dumps(reports)

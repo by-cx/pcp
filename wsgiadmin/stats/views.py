26b6cc7ba8d65f957a153af468d78ddb51857990
Adam Strauch <cx@initd.cz>
2011-11-27 03:48:33 +0100

    Credit's stats

--- wsgiadmin/stats/views.py
+++ wsgiadmin/stats/views.py
@@ -1,11 +1,13 @@
+from datetime import date, timedelta
 from constance import config
 from django.contrib import messages
 from django.core.urlresolvers import reverse
+from django.db.models.aggregates import Min, Sum
 from django.http import HttpResponseRedirect
 from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from wsgiadmin.stats.models import Credit
+from wsgiadmin.stats.models import Credit, Record
 from django.utils.translation import ugettext_lazy as _
 
 class CreditView(TemplateView):
@@ -41,4 +43,56 @@ class CreditView(TemplateView):
         context["for_three_months_cost"] = (self.user.parms.pay_total_day() * 90) / self.user.parms.one_credit_cost
         context["for_six_months_cost"] = (self.user.parms.pay_total_day() * 180) / self.user.parms.one_credit_cost
         context["for_year_cost"] = (self.user.parms.pay_total_day() * 360) / self.user.parms.one_credit_cost
+        return context
+
+class StatsView(TemplateView):
+    template_name = "bill.html"
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        self.user = request.session.get('switched_user', request.user)
+        return super(StatsView, self).dispatch(request, *args, **kwargs)
+
+    def month_stats(self):
+        today = date.today()
+        objs = self.user.record_set.aggregate(Min("date"))
+        first_day = objs["date__min"] if objs else date.today()
+        month = first_day.month
+        year = first_day.year
+        data = []
+        while year <= today.year:
+            while month <= today.month:
+                res = self.user.record_set.filter(date__year=year, date__month=month).aggregate(Sum("cost"))
+                if res:
+                    data.append({"label": "%d/%d" % (month, year), "cost": res["cost__sum"]})
+                else:
+                    data.append({"label": "%d/%d" % (month, year), "cost": 0.0})
+                if month == 12:
+                    year += 1
+                    month = 0
+                month += 1
+            year += 1
+        return data
+
+    def day_stats(self):
+        records = self.user.record_set.filter(date__gte=date.today() - timedelta(14)).values("date", "cost").extra(select={"date": "date", "cost": "SUM(cost)"})
+        records.query.group_by = ["date"]
+        records.order_by("date")
+        data = []
+        for record in records.all():
+            data.append(record)
+        return data
+
+    def buyed_stats(self):
+        return self.user.credit_set.order_by("date")
+
+    def get_context_data(self, **kwargs):
+        context = super(StatsView, self).get_context_data(**kwargs)
+        context["months"] = self.month_stats()
+        context["days"] = self.day_stats()
+        context["credits"] = self.buyed_stats()
+        context['u'] = self.user
+        context['superuser'] = self.request.user
+        context['menu_active'] = "dashboard"
+        context['config'] = config
         return context

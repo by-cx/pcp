ddfafb6b33288d736bba3f4b99ac23a9de2d4c49
yed_ <yed@vanyli.net>
2012-02-18 16:39:06 +0100

    able to run tests

--- pavement.py
+++ pavement.py
@@ -1,10 +1,14 @@
 from __future__ import with_statement
+
+import sys
 import os
+
+
 from os.path import join, exists
 from subprocess import check_call
 
 from paver.easy import (
-    task, needs, call_task, sh, options,
+    task, needs, call_task, sh, options, consume_args
 )
 from paver.setuputils import setup
 
@@ -75,19 +79,19 @@ def translate(options):
     locales = ('cs', )
 
     curdir = os.getcwd()
-    proot = join(curdir, PROJECT_ROOT)
-    os.chdir(proot)
+    root_dir = join(curdir, PROJECT_ROOT)
+    os.chdir(root_dir)
 
     if common_trans:
-        if not os.path.exists(join(proot, 'locale')):
-            os.mkdir(join(proot, 'locale'))
+        if not os.path.exists(join(root_dir, 'locale')):
+            os.mkdir(join(root_dir, 'locale'))
     else:
-        for root, dirs, files in os.walk(proot):
+        for root, dirs, files in os.walk(root_dir):
             if 'models.py' in files and 'locale' not in dirs:
-                os.mkdir(join(proot, root, 'locale'))
+                os.mkdir(join(root_dir, root, 'locale'))
                 for one in locales:
                     print 'mkdir in %s' % join(root, 'locale', one)
-                    os.mkdir(join(proot, root, 'locale', one))
+                    os.mkdir(join(root_dir, root, 'locale', one))
 
     from django.core.management import call_command
     for one in locales:
@@ -98,8 +102,8 @@ def translate(options):
 @task
 def compile_translations(options):
     curdir = os.getcwd()
-    proot = join(curdir, PROJECT_ROOT)
-    os.chdir(proot)
+    root_dir = join(curdir, PROJECT_ROOT)
+    os.chdir(root_dir)
 
     from django.core.management import call_command
     call_command('compilemessages')
@@ -108,8 +112,8 @@ def compile_translations(options):
 @task
 def remove_source_translations(options):
     curdir = os.getcwd()
-    proot = join(curdir, PROJECT_ROOT)
-    os.chdir(proot)
+    root_dir = join(curdir, PROJECT_ROOT)
+    os.chdir(root_dir)
 
     for lang in os.listdir("locale"):
         for file in os.listdir(join("locale", lang)):
@@ -144,3 +148,48 @@ def upload_package(options):
 def deploy(options):
     call_task('upload_package')
     check_call(['fab', 'deploy'])
+
+
+def djangonize_test_environment(test_project_module):
+
+    root_dir = os.getcwd()
+
+    sys.path.insert(0, root_dir)
+    sys.path.insert(0, join(root_dir, "tests"))
+
+    if exists(join(root_dir, "tests", test_project_module)):
+        sys.path.insert(0, join(root_dir, "tests", test_project_module))
+
+    os.environ['DJANGO_SETTINGS_MODULE'] = "%s.settings" % test_project_module
+
+def run_tests(test_project_module, nose_args, nose_run_kwargs=None):
+    djangonize_test_environment(test_project_module)
+
+    import nose
+
+    root_dir = os.getcwd()
+
+    os.chdir(join(root_dir, "tests", test_project_module))
+
+    argv = ["--with-django"] + nose_args
+
+    nose_run_kwargs = nose_run_kwargs or {}
+
+    nose.run_exit(
+        argv = ["nosetests"] + argv,
+        defaultTest = test_project_module,
+        **nose_run_kwargs
+    )
+
+@task
+@consume_args
+def unit(args, nose_run_kwargs=None):
+    """ Run unittests """
+    run_tests(test_project_module="unit_project", nose_args=args, nose_run_kwargs=nose_run_kwargs)
+
+
+@task
+@consume_args
+def integrate(args, nose_run_kwargs=None):
+    """ Run integration tests """
+    run_tests(test_project_module="integrate_project", nose_args=["--with-selenium", "--with-djangoliveserver"]+args, nose_run_kwargs=nose_run_kwargs)

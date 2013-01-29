b87fe3f0cbb26d5cac9b086537d200d70e95b2b7
Adam Strauch <cx@initd.cz>
2013-01-29 23:58:15 +0100

    Domain form fix (emails)

--- wsgiadmin/emails/forms.py
+++ wsgiadmin/emails/forms.py
@@ -55,6 +55,7 @@ class DomainForm(forms.ModelForm):
     def clean_name(self):
         if not re.match("^[0-9a-zA-Z\.]*$", self.cleaned_data["name"]):
             raise forms.ValidationError(_("Domain name has to be in this format: ^[0-9a-zA-Z\.]*$"))
+        return self.cleaned_data["name"]
 
     def clean_user(self):
         return None

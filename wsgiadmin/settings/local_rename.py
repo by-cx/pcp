2f0563484dea292aa0f4e0e47490803999743650
vlinhart <vladimir.linhart@gmail.com>
2013-05-19 23:20:24 +0200

    minor typo changes, missing requirement, placeholder local settings file for easier start

---
+++ wsgiadmin/settings/local_rename.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+GOPAY_SECRET_KEY = 'test'
+GOPAY_GOID = 'test'
+GOPAY_DEFAULT_PAYMENT_CHANNEL = 'test'
+GOPAY_LANG = 'test'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.db',
+    },
+}
